52db8ac10c24e591ddc29b51bdf5ed18836fc4a8
maxx0501 <mateused0501@gmail.com>
2023-05-29 16:58:25 -0300

    responsividade 90%

--- resources/views/contate-nos.blade.php
+++ resources/views/contate-nos.blade.php
@@ -12,17 +12,22 @@ Contato
       <h1>Entre em Contato</h1>
       <form class="contact-form" action="enviar-email.php" method="post">
           <label for="nome">Nome:</label>
+          
           <input type="text" id="nome" name="nome" required>
+          <br>
           <br>
           <label for="email">E-mail:</label>
           <input type="email" id="email" name="email" required>
           <br>
+          <br>
           <label for="assunto">Assunto:</label>
           <input type="text" id="assunto" name="assunto" required>
+          <br>
           <br>
           <label for="mensagem">Mensagem:</label>
           <textarea id="mensagem" name="mensagem" required></textarea>
           <br>
+          
           <button type="submit" class="inline-btn">Enviar</button>
       </form>
       </div>
